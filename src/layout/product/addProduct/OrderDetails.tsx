570fe0a6a9b9bce95f8ba58bdd8b6594520e799b
SWE-Jahidul <programmerjahid162@gmaik.com>
2024-10-01 03:36:00 +0600

    Updated: Field

--- src/layout/product/addProduct/OrderDetails.tsx
+++ src/layout/product/addProduct/OrderDetails.tsx
@@ -1,56 +1,124 @@
 export default function OrderDetails() {
   return (
     <div className="text-black mt-7">
-      <div className="flex sm:flex-col md:flex-row mb-7 ">
+      <div className="flex sm:flex-col md:flex-row mb-3 ">
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold uppercase tracking-wider mb-1">Order No:</h3>
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Order No:
+          </h3>
           <h1 className="text-2xl text-gray-800 ">10 Abdul</h1>
         </div>
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold  uppercase tracking-wider mb-1">Total Quantity:</h3>
+          <h3 className="text-sm font-semibold  uppercase tracking-wider ">
+            Total Quantity:
+          </h3>
           <h1 className="text-2xl text-gray-800">1</h1>
         </div>
       </div>
-      <div className="flex sm:flex-col md:flex-row  mb-7">
+      <div className="flex sm:flex-col md:flex-row  mb-3">
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold  uppercase tracking-wider mb-1">Origin Facilty:</h3>
+          <h3 className="text-sm font-semibold  uppercase tracking-wider ">
+            Origin Facilty:
+          </h3>
           <h1 className="text-2xl text-gray-800">Calgory</h1>
         </div>
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold  uppercase tracking-wider mb-1">Destination facility:</h3>
+          <h3 className="text-sm font-semibold  uppercase tracking-wider ">
+            Destination facility:
+          </h3>
           <h1 className="text-2xl text-gray-800">Calgory</h1>
         </div>
       </div>
-      <div className="flex sm:flex-col md:flex-row  mb-7  ">
+      <div className="flex sm:flex-col md:flex-row  mb-3  ">
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold  uppercase tracking-wider mb-1">Coustomer Name:</h3>
+          <h3 className="text-sm font-semibold  uppercase tracking-wider ">
+            Coustomer Name:
+          </h3>
           <h1 className="text-2xl text-gray-800">Abdul</h1>
         </div>
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold  uppercase tracking-wider mb-1">Customer Address:</h3>
+          <h3 className="text-sm font-semibold  uppercase tracking-wider ">
+            Customer Address:
+          </h3>
           <h1 className="text-2xl text-gray-800">1 Bangladesh</h1>
         </div>
       </div>
-      <div className="flex sm:flex-col md:flex-row mb-7">
+      <div className="flex sm:flex-col md:flex-row mb-3">
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold  uppercase tracking-wider mb-1">Customer Alternate Address:</h3>
-          <h1  className="text-2xl text-gray-800">2 Bangladesh</h1>
+          <h3 className="text-sm font-semibold  uppercase tracking-wider ">
+            Customer Alternate Address:
+          </h3>
+          <h1 className="text-2xl text-gray-800">2 Bangladesh</h1>
         </div>
         <div>
-          <h3 className="text-sm font-semibold uppercase tracking-wider mb-1">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
             City:
           </h3>
           <h1 className="text-2xl text-gray-800">Bangladesh</h1>
         </div>
       </div>
-      <div className="flex sm:flex-col md:flex-row  mb-7">
+      <div className="flex sm:flex-col md:flex-row  mb-3">
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold uppercase tracking-wider mb-1">Customer Postal Code:</h3>
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Customer Postal Code:
+          </h3>
           <h1 className="text-2xl text-gray-800">THB26E</h1>
         </div>
         <div className="text-left w-1/2">
-          <h3 className="text-sm font-semibold uppercase tracking-wider mb-1">Customer Province:</h3>
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Customer Province:
+          </h3>
           <h1 className="text-2xl text-gray-800">NU</h1>
+        </div>
+      </div>
+      <div className="flex sm:flex-col md:flex-row  mb-3">
+        <div className="text-left w-1/2">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Customer Email:
+          </h3>
+          <h1 className="text-2xl text-gray-800">Abdul@mgmail.com</h1>
+        </div>
+        <div className="text-left w-1/2">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Customer Mobile:
+          </h3>
+          <h1 className="text-2xl text-gray-800">4167920088</h1>
+        </div>
+      </div>
+      <div className="flex sm:flex-col md:flex-row  mb-3">
+        <div className="text-left w-1/2">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Customer Phone Number:
+          </h3>
+          <h1 className="text-2xl text-gray-800">4167920088</h1>
+        </div>
+        <div className="text-left w-1/2">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Warehouse Name:
+          </h3>
+          <h1 className="text-2xl text-gray-800">Calgary</h1>
+        </div>
+      </div>
+      <div className="flex sm:flex-col md:flex-row  mb-3">
+        <div className="text-left w-1/2">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Created At:
+          </h3>
+          <h1 className="text-2xl text-gray-800">9/30/2024, 3:25:28 PM</h1>
+        </div>
+        <div className="text-left w-1/2">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Updated At:
+          </h3>
+          <h1 className="text-2xl text-gray-800">9/30/2024, 3:25:28 PM</h1>
+        </div>
+      </div>
+      <div className="flex sm:flex-col md:flex-row  mb-3">
+        <div className="text-left w-full">
+          <h3 className="text-sm font-semibold uppercase tracking-wider ">
+            Status
+          </h3>
+          <h1 className="text-2xl text-gray-800">New</h1>
         </div>
       </div>
     </div>
